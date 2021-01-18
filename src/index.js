c0e5fa48849f4aeabd0f6d46d27ce4763f2f6e30
salim ameur <salim.ameur@gmail.com>
2021-01-18 01:04:39 +0100

    index

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>HELLO THERE</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
